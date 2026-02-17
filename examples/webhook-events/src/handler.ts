4709839cd84db2865e9d37baaeed42c804a9cc0d
The-LukeZ <contact.lukez@proton.me>
2026-02-17 22:23:00 +0100

    Update honocord version in package.json and pnpm-lock.yaml; enhance error handling in webhook event handler

--- examples/webhook-events/src/handler.ts
+++ examples/webhook-events/src/handler.ts
@@ -1,24 +1,54 @@
-import { REST } from "@discordjs/rest";
+import { DiscordAPIError } from "@discordjs/rest";
 import {
   APIDMChannel,
   ApplicationWebhookEventType,
   Routes,
 } from "discord-api-types/v10";
-import { WebhookEventHandler } from "honocord";
+import { REST, WebhookEventHandler, AttachmentBuilder } from "honocord";
 
-const handler = new WebhookEventHandler<true, Cloudflare.Env>(
+const handler = new WebhookEventHandler<
+  ApplicationWebhookEventType.ApplicationAuthorized,
+  Cloudflare.Env,
+  {},
+  true
+>(
   ApplicationWebhookEventType.ApplicationAuthorized,
   true, // Enable worker mode
 ).addHandler(async (c) => {
-  const { data } = c.get("data");
+  const { data } = c.var.data;
 
   const rest = new REST().setToken(c.env.DISCORD_TOKEN);
 
-  (await rest.post(Routes.userChannels(), {
-    body: {
-      recipient_id: "123456789012345678",
-    },
-  })) as APIDMChannel;
+  let dm: APIDMChannel | undefined;
+  try {
+    dm = (await rest.post(Routes.userChannels(), {
+      body: {
+        recipient_id: "123456789012345678",
+      },
+    })) as APIDMChannel;
+  } catch (error) {
+    if (error instanceof DiscordAPIError)
+      console.error("Discord API Error:", error);
+    else console.error("Unexpected Error:", error);
+  }
 
-  // No return statement required!
+  // just in case
+  if (!dm) {
+    console.error("Failed to create DM channel");
+    return;
+  }
+
+  const attachment = new AttachmentBuilder(
+    Buffer.from("Hello, world!"),
+  ).setName("hello.txt");
+  const { files, attachments } = attachment.resolve();
+
+  await rest.post(Routes.channelMessages(dm.id), {
+    body: {
+      content:
+        "Hello! This is a message sent in response to the Application Authorized event.",
+      attachments,
+    },
+    files,
+  });
 });
