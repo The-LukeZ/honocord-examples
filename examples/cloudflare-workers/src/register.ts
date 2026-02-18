b88640211d30eb018576961e90cdba3de7bc823c
The-LukeZ <contact.lukez@proton.me>
2026-02-18 10:13:16 +0100

    Fix syntax error by adding a trailing comma in registerCommands call

--- examples/cloudflare-workers/src/register.ts
+++ examples/cloudflare-workers/src/register.ts
@@ -4,5 +4,5 @@ import * as handlers from "./handlers";
 await registerCommands(
   process.env.DISCORD_TOKEN!,
   process.env.DISCORD_APPLICATION_ID!,
-  ...Object.values(handlers)
+  ...Object.values(handlers),
 );
